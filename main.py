50d6e3f303cd564a580556347f137484484a81ff
Xushnudd <boshhacker1@gmail.com>
2025-07-12 13:45:23 +0500

    3-tushunilmadi

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from csv import reader
 
 # Define function to retrieve prices colum in to a list
 def get_prices(data):
@@ -11,6 +11,11 @@ def get_prices(data):
     Returns:
         list: list of prices
     """
+    f = open(data)
+    f = reader(f)
+    for i in f:
+        print(i[2])
+print(get_prices("data.csv"))
 
 
 def get_products(data):
@@ -23,7 +28,11 @@ def get_products(data):
     Returns:
         list: list of products
     """
-
+    f = open(data)
+    f = reader(f)
+    for i in f:
+        print(i[0])
+print(get_products("data.csv"))
 def get_expensive(prices):
     """
     Finds the most expensive product of index
@@ -34,8 +43,14 @@ def get_expensive(prices):
     Returns:
         int: index of most expensive product
     """
-
-    
+    f = open(prices)
+    f = reader(f)
+    s = ""
+    for i in f:
+        if s < i[2][1::]:
+            s = i[2][1::]
+    return s
+print(get_expensive("data.csv"))
 
 # Read data from file
 f = open("data.csv")
